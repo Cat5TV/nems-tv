800521f5da6a2660a3a12929c58a037308f19f59
NEMS Linux <nems@baldnerd.com>
2020-06-01 13:08:39 -0400

    Enable speedtest log on 1.6

--- connectors/speedtest.php
+++ connectors/speedtest.php
@@ -1,6 +1,7 @@
 <?php
   // Interpret the speedtest cache log to output on NEMS TV Dashboard
-  // Requires NEMS Linux 1.6+
+  // Will not show up until NEMS Linux 1.6 since /var/log/nems is owned by root
+  // Can chown to nagios:nagios to correct that, if desired
   if (file_exists('/var/log/nems/speedtest.log')) {
     $speedtest = file('/var/log/nems/speedtest.log');
   }
@@ -12,7 +13,6 @@
     } elseif ($speedtest[0] == 2) { // CRIT
       $color = '#ff686b';
     }
-//    echo '<b style="color:#fff;">Internet Status:</b><br />';
     echo '<span style="color:#777;">Ping: <span style="color:' . $color . '">' . $speedtest[1] . $speedtest[2] . '</span> <i class="fa fa-arrow-down"></i> <span style="color:' . $color . '">' . $speedtest[3] . $speedtest[4] . '</span> <i class="fa fa-arrow-up"></i> <span style="color:' . $color . '">' . $speedtest[5] . $speedtest[6] . '</span></span>';
   }
 ?>
